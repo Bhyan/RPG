1d02c0b9ad79ed09e21658edf02d7bd355eb51ea
bhyan <bhyanbrito@gmail.com>
2016-07-15 17:17:33 -0300

    Att

--- app/RPG.py
+++ app/RPG.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 import kivy
 kivy.require('1.7.0')
-
-
 from kivy.app import App
 from kivy.uix.widget import Widget
 
-class exp5a(App):
+
+class rpg(App):
 	pass
 
 if __name__ == '__main__':
-	exp5a().run()    
+	rpg().run()    
